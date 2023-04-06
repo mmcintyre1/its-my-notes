462d6dcc49439bb558da04901b849b1d5080edac
Mike <mfmcintyre01@gmail.com>
2023-04-06 12:33:13 -0400

    updated script to copy to clipboard

--- scripts/get_pretty_date.sh
+++ scripts/get_pretty_date.sh
@@ -1,3 +1,9 @@
-#/bin/bash
+#!/bin/bash
 
-date +"%F %T.%6N"
+# get the date in the desired format
+date_formatted=$(date +"%F %T.%6N")
+
+# copy the formatted date to the clipboard using clip.exe
+echo -n "$date_formatted" | clip.exe
+
+echo "Formatted date copied to clipboard."
